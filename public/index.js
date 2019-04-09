9bdbeb2ba8d2d1dc2147f8128d4254b8892cde48
Aleksander Budzynowski <998324+aleksb@users.noreply.github.com>
2019-04-09 15:58:28 +1000

    start of BST recursive insert for balanced tree

--- public/index.js
+++ public/index.js
@@ -1,77 +1,25 @@
+import { blockStatement } from "@babel/types";
+
 // setup
 const source   = document.getElementById("entry-template").innerHTML;
 const template = Handlebars.compile(source);
 
-// data
-// const context = {title: "My New Post", body: "This is my first post!"};
-
 const context = {
-    /*
-    logs: [
-        {
-            id:         1,
-            severity:   "low",
-            reportType: "script",
-            timestamp:  15243627
-        },
-        {
-            id:         2,
-            severity:   "critical",
-            reportType: "mixed content",
-            timestamp:  038317927
-        },
-        {
-            id:         3,
-            severity:   "moderate",
-            reportType: "style-src",
-            timestamp:  1554431444,
-        },
-        {
-            id:         4,
-            severity:   "severe",
-            reportType: "script-src",
-            timestamp:  1554431448,
-        },
-        {
-            id:         5,
-            severity:   "High",
-            reportType: "Violated Directive",
-            timestamp:  1554431256
-        },
-        {
-            id:         6,
-            severity:   "Medium",
-            reportType: "Mixed Content",
-            timestamp:  1554431278
-        },
-        {
-            id:         7,
-            severity:   "high",
-            reportType: "script",
-            timestamp:  15649483904
-        }, 
-        {
-            id:         8,
-            severity:   "low",
-            reportType: "mixed content",
-            timestamp:  15649483905
-        },
-        {
-            id:         9,
-            severity:   "medium",
-            reportType: "script",
-            timestamp:  1554431249
-        },
-        {
-            id:         10,
-            severity:   "high",
-            reportType: "style",
-            timestamp:  1554431375
-        }
-    ]*/
-
     logs: logList,
 };
+
+
+const bTree = new BST();
+
+function addToBtree(array, first, last) {
+    const medianIndex = Math.floor((first + last) / 2);
+
+    bTree.insert(array[medianIndex].timestamp, array[medianIndex]);
+
+    
+}
+
+addToBtree(logList, 0, logList.length - 1);
 
 
 document.getElementById("display").innerHTML += template(context);
